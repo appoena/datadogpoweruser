010e010a92b484cf39cd12d94e1bad56040b0764
willianvalerio <willianvalerio@outlook.com>
2021-12-30 21:04:13 -0300

    Add python logs

--- apm/python/src/app.py
+++ apm/python/src/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
 import requests
+import logging
+import json_log_formatter
 
-app = Flask(__name__)
+
+app = Flask("python-flask")
+# Disable Flask's default logging
+werkzeug = logging.getLogger('werkzeug')
+werkzeug.disabled = True
+
+# Set up a custom logger
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.StreamHandler()
+json_handler.setFormatter(json_log_formatter.VerboseJSONFormatter())
+json_handler.setLevel(logging.INFO)
+log.addHandler(json_handler)
+app.logger.propagate = False
+log.propagate = False
 
 @app.route('/')
 def hello_world():
@@ -9,12 +26,14 @@ def hello_world():
 
 @app.route('/api')
 def rest_hello_world():
+    log.info("hello world - appoena")
     return '{"id":1,"message":"Flask: Hello World"}'
 
 
 @app.route('/api/dotnet')
 def rest_external():
+    log.info('Calling external API')
     return requests.get('http://dotnet-todoapi:8081/api/todoitems').text
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
