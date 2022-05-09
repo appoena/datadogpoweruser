0ed3c67c0e3552820f07fe817e85625aeb5ea57d
willianvalerio <willianvalerio@outlook.com>
2022-05-09 10:59:08 -0300

    Add serverless framework

---
+++ serverless/serverless-framework/populate.sh
@@ -0,0 +1,5 @@
+id_apigateway=""
+
+curl https://$id_apigateway.execute-api.us-east-1.amazonaws.com/datadogpoweruser/
+curl https://$id_apigateway.execute-api.us-east-1.amazonaws.com/datadogpoweruser/hello
+curl https://$id_apigateway.execute-api.us-east-1.amazonaws.com/datadogpoweruser/erro
